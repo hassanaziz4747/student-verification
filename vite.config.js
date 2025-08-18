f497a3d7f2804ac687b85d657091d3262f78436c
Hassan Aziz <somewhere@email.com>
2025-08-18 18:44:34 +0500

    deployment

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,6 @@ export default defineConfig({
 	server: {
 		host: true,
 		port: 5173,
-		strictPort: true,
-		allowedHosts: ['vtleqb-ip-103-169-64-93.tunnelmole.net'],
+		strictPort: true
 	}
 });
